5524d67d89cf098c40d73c740827bb4eb7cac4bc
Danilo Valério da Silva <danilo_vs@hotmail.com>
2022-05-28 09:29:12 -0300

    CRUD

--- demo-teste/src/main/java/com/danilo/projetos/demoteste/repository/ProdutoRepository.java
+++ demo-teste/src/main/java/com/danilo/projetos/demoteste/repository/ProdutoRepository.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 
 import com.danilo.projetos.demoteste.model.Produto;
 
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -12,8 +15,68 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class ProdutoRepository {
 
-    private ArrayList<Produto> produtos = new ArrayList<>();
+    private List<Produto> produtos = new ArrayList<>();
     private Integer ultimoId = 0;
 
-    
+    /**
+     * Metodo para retornar uma lista de produtos
+     *
+     * @return Lista de produtos.
+     */
+    public List<Produto> obterTodos() {
+        return produtos;
+    }
+
+    /**
+     * Metodo que retorna um Optional de Produto pelo seu id
+     *
+     * @param id id do produto procurado
+     * @return retorna um Produto caso seja encontrado
+     */
+    public Optional<Produto> obterPorId(Integer id) {
+        return produtos
+                .stream()
+                .filter(produto -> produto.getId() == id)
+                .findFirst();
+    }
+
+    /**
+     * Incrementa valor no ultimo id e adiciona produto na lista
+     * @param produto que será adicionado
+     * @return retorna produto adicionado na lista
+     */
+    public Produto adicionar(Produto produto) {
+        ultimoId++;
+        produto.setId(ultimoId);
+        produtos.add(produto);
+
+        return produto;
+    }
+
+    /**
+     * Remove produto com o id informado
+     * @param id Id do produto que será deletado
+     */
+    public void deletar(Integer id) {
+        produtos.removeIf(produto -> produto.getId() == id);
+    }
+
+    /**
+     * Atualiza produto na lista
+     * @param produto Produto a ser atualizado
+     * @return produto atualizado
+     */
+    public Produto atualizar(Produto produto) {
+        Integer indexOfProduct = produtos.indexOf(produto);
+
+        Optional<Produto> produtoEncontrado = obterPorId(produto.getId());
+
+        if (produtoEncontrado.isEmpty()) {
+            throw new InputMismatchException("Produto não encontrado");
+        }
+
+        produtos.set(indexOfProduct, produto);
+        return produto;
+    }
+
 }
