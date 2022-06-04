be36508659cb95fd96c8f64d6f2d4c1a81b8e9e6
Danilo Valério da Silva <danilo_vs@hotmail.com>
2022-06-04 04:20:54 -0300

    notacoes entity id e generatedvalue

--- demo-teste/src/main/java/com/danilo/projetos/demoteste/model/Produto.java
+++ demo-teste/src/main/java/com/danilo/projetos/demoteste/model/Produto.java
@@ -1,7 +1,15 @@
 package com.danilo.projetos.demoteste.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Produto {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private String nome;
     private Integer quantidade;
